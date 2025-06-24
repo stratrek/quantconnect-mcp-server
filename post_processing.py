a4b33b01f860055eb989ebe68e1e611412a15c52
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-06-24 11:02:33 -0600

    Update post_processing.py

--- post_processing.py
+++ post_processing.py
@@ -1,1 +1,15 @@
-print("Call post_processing.py")
+path = 'src/models.py'
+
+# Read the file content
+with open(path, 'r', encoding='utf-8') as file:
+    content = file.read()
+
+# Add the extra import.
+content = 'from pydantic import RootModel\n' + content
+
+# Perform string replacements.
+content = content.replace('__root__', 'RootModel').replace('ResponseModel', 'Response')
+
+# Save the new file content.
+with open(path, 'w', encoding='utf-8') as file:
+    file.write(content)
