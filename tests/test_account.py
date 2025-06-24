5354c9615c3785a22bec1db6bca7fc40e3503842
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-06-24 17:14:53 -0600

    Update test_account.py

--- tests/test_account.py
+++ tests/test_account.py
@@ -10,7 +10,7 @@ class TestAccount:
 
     @pytest.mark.asyncio
     async def test_read_account(self):
-        assert False os.getenv('QUANTCONNECT_USER_ID')
+        assert False, os.getenv('QUANTCONNECT_USER_ID')
         result = await mcp.call_tool('read_account', {})
         assert result[0].success, result
         assert AccountResponse.model_validate(loads(result[0].text))
