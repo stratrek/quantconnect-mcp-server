d99755fd9f7e72107fe5b789e7f912d3f741adf7
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-06-24 11:11:42 -0600

    Create api_connection.py

---
+++ src/api_connection.py
@@ -0,0 +1,45 @@
+import httpx
+from base64 import b64encode
+from hashlib import sha256
+from time import time
+import os
+from pydantic_core import to_jsonable_python
+
+# Load credentials from environment variables.
+USER_ID = os.getenv('QUANTCONNECT_USER_ID')
+API_TOKEN = os.getenv('QUANTCONNECT_API_TOKEN')
+
+def get_headers():
+    # Get timestamp
+    timestamp = f'{int(time())}'
+    time_stamped_token = f'{API_TOKEN}:{timestamp}'.encode('utf-8')
+    # Get hased API token
+    hashed_token = sha256(time_stamped_token).hexdigest()
+    authentication = f'{USER_ID}:{hashed_token}'.encode('utf-8')
+    authentication = b64encode(authentication).decode('ascii')
+    # Create headers dictionary.
+    return {
+        'Authorization': f'Basic {authentication}',
+        'Timestamp': timestamp
+    }
+
+async def post(endpoint: str, model: object = None):
+    """Make an HTTP POST request to the API with proper error handling.
+    
+    Args:
+        endpoint: The API endpoint path (ex: '/projects/create')
+        model: Optional Pydantics model for the request.
+        
+    Returns:
+        Response JSON if successful. Otherwise, throws an exception, 
+        which is handled by the Server class.
+    """
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            f'https://www.quantconnect.com/api/v2{endpoint}', 
+            headers=get_headers(), 
+            json=to_jsonable_python(model, exclude_none=True) if model else {}, 
+            timeout=10.0
+        )
+        response.raise_for_status()
+        return response.json()
