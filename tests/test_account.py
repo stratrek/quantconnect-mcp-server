eea1365349a2f5bb336e362208f7d330b243e8da
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-06-24 15:34:49 -0600

    Create test_account.py

---
+++ tests/test_account.py
@@ -0,0 +1,13 @@
+import pytest
+from json import loads
+
+from main import mcp
+from models import AccountResponse
+
+
+class TestAccount:
+
+    @pytest.mark.asyncio
+    async def test_read_account(self):
+        result = await mcp.call_tool('read_account', {})
+        assert AccountResponse.model_validate(loads(result[0].text))
