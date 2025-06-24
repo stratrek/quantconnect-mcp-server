c9cbbde8f9aba7dd8c16ce1f1d61b31a009685b0
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-06-24 15:44:58 -0600

    Update post_processing.py

--- post_processing.py
+++ post_processing.py
@@ -1,14 +1,26 @@
 path = 'src/models.py'
 
-# Read the file content
+# Read the file content.
 with open(path, 'r', encoding='utf-8') as file:
-    content = file.read()
+    lines = file.readlines()
 
-# Add the extra import.
-content = 'from pydantic import RootModel\n' + content
+# Add the extra import (after `from __future__` to avoid errors).
+lines.insert(5, 'from pydantic import RootModel, ConfigDict\n')
+content = ''.join(lines)
 
 # Perform string replacements.
 content = content.replace('__root__', 'RootModel').replace('ResponseModel', 'Response')
+
+# Replace
+# ```
+#    class Config:
+#        extra = Extra.forbid
+# ```
+# with 
+# `model_config = ConfigDict(extra='forbid')`
+# to avoid warnings when running pytest.
+content = content.replace('class Config:', "model_config = ConfigDict(extra='forbid')")\
+    .replace('        extra = Extra.forbid': '')
 
 # Save the new file content.
 with open(path, 'w', encoding='utf-8') as file:
