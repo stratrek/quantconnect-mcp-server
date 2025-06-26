9ebb9ed23c55881cc4732d928eb57521357a3a6e
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-06-26 13:59:11 -0600

    Create utils.py

---
+++ tests/utils.py
@@ -0,0 +1,22 @@
+import pytest
+from json import loads
+from pydantic import ValidationError
+
+async def validate_models(
+        mcp, tool_name, input_args={}, input_class=None, output_class=None):
+    if input_class:
+        # The following line ensures we can convert the input dict to 
+        # the input Pydantic model. If required arguments are missing 
+        # or arguments are invalid, it raises an exception.
+        input_model = input_class.model_validate(input_args)
+    else:
+        input_model = {}
+    result = await mcp.call_tool(tool_name, input_model)
+    output_args = loads(result[0].text)
+    assert output_args['success'], output_args
+    return output_class.model_validate(output_args)
+
+async def raises_validation_error(tool_name, args, cls):
+    with pytest.raises(ValidationError) as error:
+        cls.model_validate(args)
+    assert error.type is ValidationError
