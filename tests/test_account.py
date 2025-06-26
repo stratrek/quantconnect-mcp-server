c14888356ce51063438b0e2b7c1233aa309022b7
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-06-26 13:59:40 -0600

    Update test_account.py

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,7 +1,7 @@
 import pytest
-from json import loads
 
 from main import mcp
+from utils import validate_models
 from models import AccountResponse
 
 
@@ -9,7 +9,6 @@ class TestAccount:
 
     @pytest.mark.asyncio
     async def test_read_account(self):
-        result = await mcp.call_tool('read_account', {})
-        result = loads(result[0].text)
-        assert result['success'], result
-        assert AccountResponse.model_validate(result), 'Failed Pydantic conversion'
+        await validate_models(
+            mcp, 'read_account', output_class=AccountResponse
+        )
