c5a49b1be2c1f97943476b40cb54fe82460043de
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-07-02 11:45:39 -0600

    Update utils.py

--- tests/utils.py
+++ tests/utils.py
@@ -2,14 +2,16 @@ import pytest
 import jsonschema
 from json import loads
 from pydantic import ValidationError
+from datetime import datetime
 
 async def validate_models(mcp, tool_name, input_args={}, output_class=None):
-    # Run the tool. If the input args are invalid, it raises an error.
-    result = await mcp.call_tool(tool_name, input_args) # a tuple
-    text_contents = result[0] # A list of TextContent objects
-    structured_response = result[1]
+    # Call the tool with the arguments. If the input args are invalid,
+    # it raises an error.
+    unstructured_response, structured_response = await mcp.call_tool(
+        tool_name, {'model': input_args} if input_args else {}
+    )
     # Check if the response has the success flag.
-    assert loads(text_contents[0].text)['success'], 'Request failed'    
+    assert loads(unstructured_response[0].text)['success'], structured_response
     # Check if the response schema is valid JSON.
     jsonschema.validate(
         instance=structured_response, 
@@ -20,5 +22,8 @@ async def validate_models(mcp, tool_name, input_args={}, output_class=None):
 
 async def raises_validation_error(tool_name, args, cls):
     with pytest.raises(ValidationError) as error:
-        cls.model_validate(args)
+        cls.model_validate({'model': args})
     assert error.type is ValidationError
+
+def create_timestamp():
+    return datetime.now().strftime('%Y%m%d_%H%M%S_%f')
