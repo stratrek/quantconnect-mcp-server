25e8f35fca2a08845ac513191902db22c2473302
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-07-03 14:06:11 -0600

    Update test_project.py

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,4 +1,5 @@
 import pytest
+import functools
 
 from main import mcp
 from utils import (
@@ -16,30 +17,35 @@ from models import (
 )
 
 
-class TestProject:
+# Static helpers for common operations:
+class Project:
 
-    # Static helpers for common operations:
     @staticmethod
-    async def create(payload):
+    async def create(name=f"Project {create_timestamp()}", language='Py'):
         output_model = await validate_models(
-            mcp, 'create_project', payload, ProjectListResponse
+            mcp, 'create_project', {'name': name, 'language': language},
+            ProjectListResponse
         )
         return output_model.projects[0]
 
     @staticmethod
-    async def read(payload):
+    async def read(id_):
         output_model = await validate_models(
-            mcp, 'read_project', payload, ProjectListResponse
+            mcp, 'read_project', {'projectId': id_}, ProjectListResponse
         )
         return output_model.projects[0]
 
     @staticmethod
-    async def update(payload):
-        await validate_models(mcp, 'update_project', payload, RestResponse)    
+    async def update(id_, **kwargs):
+        await validate_models(
+            mcp, 'update_project', {'projectId': id_} | kwargs, RestResponse
+        )
 
     @staticmethod
-    async def delete(payload):
-        await validate_models(mcp, 'delete_project', payload, RestResponse)
+    async def delete(id_):
+        await validate_models(
+            mcp, 'delete_project', {'projectId': id_}, RestResponse
+        )
 
     @staticmethod
     async def list_():
@@ -48,28 +54,20 @@ class TestProject:
         )
         return output_model.projects
 
-    # Test suite:
+
+# Test suite:
+class TestProject:
+
     @pytest.mark.asyncio
-    @pytest.mark.parametrize(
-        'args',
-        [
-            # Case 1: Python
-            {'language': 'Py'},
-            # Case 2: C#
-            {'language': 'C#'},
-        ]
-    )
-    async def test_create_project(self, args):
+    @pytest.mark.parametrize('language', ['Py', 'C#'])
+    async def test_create_project(self, language):
         name = f"Project {create_timestamp()}"
-        language = args['language']
-        project = await TestProject.create(
-            {'name': name, 'language': language}
-        )
+        project = await Project.create(name, language)
         # Test if the name and language are correct.
         assert project.name == name
         assert project.language.value == language
         # Delete the project to clean up.
-        await TestProject.delete({'projectId': project.projectId})
+        await Project.delete(project.projectId)
 
     @pytest.mark.asyncio
     @pytest.mark.parametrize(
@@ -81,7 +79,7 @@ class TestProject:
             {'name': 'Test Project', 'language': 'C++'},
         ]
     )
-    async def test_create_project_with_invalid_args(self, args):
+    async def test_create_project_with_request_validation_errors(self, args):
         await raises_validation_error(
             'create_project', args, CreateProjectRequest
         )
@@ -91,83 +89,81 @@ class TestProject:
         # Create a project.
         name = f"Project {create_timestamp()}"
         language = 'Py'
-        project = await TestProject.create(
-            {'name': name, 'language': language}
-        )
+        id_ = (await Project.create(name, language)).projectId
         # Read the project.
-        args = {'projectId': project.projectId}
-        project = await TestProject.read(args)
+        project = await Project.read(id_)
         # Test if the name and language are correct.
         assert project.name == name
         assert project.language.value == language
         # Delete the project to clean up.
-        await TestProject.delete(args)
+        await Project.delete(id_)
 
     @pytest.mark.asyncio
     async def test_read_project_with_invalid_args(self):
         # Try to read a project that doesn't exist.
-        await raises_validation_error(
-            'read_project', {'projectId': -1}, ReadProjectRequest
+        await validate_models(
+            mcp, 'read_project', {'projectId': -1}, 
+            ReadProjectRequest, False
         )
 
     @pytest.mark.asyncio
     async def test_update_project(self):
         # Create a project.
-        name = f"Project {create_timestamp()}"
-        project = await TestProject.create({'name': name, 'language': 'Py'})
-        args = {'projectId': project.projectId}
+        id_ = (await Project.create()).projectId
         
         # Update the project name.
         new_name = f"Project {create_timestamp()}"
-        await TestProject.update(args | {'name': new_name})
+        await Project.update(id_, name=new_name)
         # Test if the new name is correct.
-        project = await TestProject.read(args)
+        project = await Project.read(id_)
         assert project.name == new_name
         
         # Update the project description.
         new_description = f"Description {create_timestamp()}"
-        await TestProject.update(args | {'description': new_description})
+        await Project.update(id_, description=new_description)
         # Test if the new description is correct.
-        project = await TestProject.read(args)
+        project = await Project.read(id_)
         assert project.description == new_description
         
         # Update the project name and description.
         new_name = f"Project {create_timestamp()}"
         new_description = f"Description {create_timestamp()}"
-        await TestProject.update(
-            args | {'name': new_name, 'description': new_description}
+        await Project.update(
+            id_, name=new_name, description=new_description
         )
         # Test if the new name & description are correct.
-        project = await TestProject.read(args)
+        project = await Project.read(id_)
         assert project.name == new_name
         assert project.description == new_description
 
         # Delete the project to clean up.
-        await TestProject.delete(args)
+        await Project.delete(id_)
 
     @pytest.mark.asyncio
     async def test_update_project_with_a_nonunique_name(self):
-        language = 'Py'
-        # Create project 1.
+        # Create 2 projects.
         name_1 = f"Project {create_timestamp()}"
-        project_1 = await TestProject.create(
-            {'name': name_1, 'language': language}
-        )
-        # Create project 2.
+        id_1 = (await Project.create(name_1)).projectId
         name_2 = f"Project {create_timestamp()}"
-        project_2 = await TestProject.create(
-            {'name': name_2, 'language': language}
-        )
+        id_2 = (await Project.create(name_2)).projectId
         # Try updating the project names to match.
-        await raises_validation_error(
-            'update_project', 
-            {'projectId': project_2.projectId, 'name': name_1}, 
-            UpdateProjectRequest
+        await validate_models(
+            mcp, 'update_project', {'projectId': id_2, 'name': name_1}, 
+            UpdateProjectRequest, False
         )
         # Delete the projects.
-        await TestProject.delete({'projectId': project_1.projectId})
-        await TestProject.delete({'projectId': project_2.projectId})
+        await Project.delete(id_1)
+        await Project.delete(id_2)
+
+    @pytest.mark.asyncio
+    async def test_delete_projects_with_invalid_args(self):
+        # Try to delete a project that doesn't exist.
+        await validate_models(
+            mcp, 'delete_project', {'projectId': -1}, 
+            DeleteProjectRequest, False
+        )
 
     @pytest.mark.asyncio
     async def test_list_projects(self):
-        await TestProject.list_()
+        await Project.list_()
+        
