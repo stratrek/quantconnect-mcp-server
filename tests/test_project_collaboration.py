9c9e04b4590bd12821d1c0c725ed7438e52161be
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-07-03 14:45:29 -0600

    Update test_project_collaboration.py

--- tests/test_project_collaboration.py
+++ tests/test_project_collaboration.py
@@ -20,6 +20,7 @@ from models import (
     DeleteCollaboratorResponse
 )
 
+COLLABORATOR_ID = os.getenv('QUANTCONNECT_COLLABORATOR_ID')
 
 class ProjectCollaboration:
 
@@ -75,7 +76,7 @@ class ProjectCollaboration:
 # Test suite:
 class TestProjectCollaboration:
 
-    _collaborator_id = os.getenv('QUANTCONNECT_COLLABORATOR_ID')
+    _collaborator_id = COLLABORATOR_ID#os.getenv('QUANTCONNECT_COLLABORATOR_ID')
 
     @pytest.mark.asyncio
     @pytest.mark.parametrize('language', ['Py', 'C#'])
@@ -83,7 +84,7 @@ class TestProjectCollaboration:
     @pytest.mark.parametrize('collaboration_write', [True, False])
     async def test_create_project_collaboration(
             self, language, collaboration_live_control, collaboration_write):
-        self._collaborator_id = os.getenv('QUANTCONNECT_COLLABORATOR_ID')
+        self._collaborator_id = COLLABORATOR_ID
         # Create a project.
         project_id = (await Project.create(language=language)).projectId
         # Add a collaborator.
