31e1ed32331c42291f7ea7435d309c90dc574cb1
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-07-04 12:08:56 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from mcp.server.fastmcp import FastMCP
 from tools.account import register_account_tools
 from tools.project import register_project_tools
 from tools.project_collaboration import register_project_collaboration_tools
+from tools.project_nodes import register_project_node_tools
 
 # Initialize the FastMCP server.
 mcp = FastMCP('quantconnect', version='0.1.0')
@@ -12,6 +13,7 @@ registration_functions = [
     register_account_tools,
     register_project_tools,
     register_project_collaboration_tools,
+    register_project_node_tools,
 ]
 for registration_function in registration_functions:
     registration_function(mcp)
