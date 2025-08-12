5934550685b4d2f4b6058a7f5a15e2f57f7bd301
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-08-12 14:43:36 -0600

    Create mcp_server_version.py

---
+++ src/tools/mcp_server_version.py
@@ -0,0 +1,12 @@
+from . import __version__
+
+def register_mcp_server_version_tools(mcp):
+    # Read
+    @mcp.tool(
+        annotations={
+            'title': 'Read QC MCP Server version', 'readOnlyHint': True
+        }
+    )
+    async def read_mcp_server_version() -> str:
+        """Returns the version of the QC MCP Server that's running."""
+        return __version__
