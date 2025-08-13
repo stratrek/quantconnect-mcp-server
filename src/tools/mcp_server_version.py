6d58c3f7c3e898dd7485ae05dca0cc3b57c3ef58
Derek Melchin <melchin.derek@gmail.com>
2025-08-13 15:09:15 -0600

    Add tool to read latest version of the server

--- src/tools/mcp_server_version.py
+++ src/tools/mcp_server_version.py
@@ -1,7 +1,9 @@
 from src import __version__
 
+import requests
+
 def register_mcp_server_version_tools(mcp):
-    # Read
+    # Read current version
     @mcp.tool(
         annotations={
             'title': 'Read QC MCP Server version', 'readOnlyHint': True
@@ -10,3 +12,19 @@ def register_mcp_server_version_tools(mcp):
     async def read_mcp_server_version() -> str:
         """Returns the version of the QC MCP Server that's running."""
         return __version__
+
+    # Read latest version
+    @mcp.tool(
+        annotations={
+            'title': 'Read latest QC MCP Server version', 'readOnlyHint': True
+        }
+    )
+    async def read_latest_mcp_server_version() -> str:
+        """Returns the latest version of the QC MCP Server released."""
+        response = requests.get(
+            "https://hub.docker.com/v2/namespaces/quantconnect/repositories/mcp-server/tags", 
+            params={"page_size": 2}
+        )
+        response.raise_for_status()
+        # Get the name of the second result. The first one is 'latest'.
+        return response.json()['results'][1]['name']
