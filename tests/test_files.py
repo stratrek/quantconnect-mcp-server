10b5dd10da7bfe175ee7a69a13f825ef3243270c
Derek Melchin <melchin.derek@gmail.com>
2025-08-14 09:41:45 -0600

    Fix test suite

--- tests/test_files.py
+++ tests/test_files.py
@@ -66,7 +66,7 @@ class Files:
         project_id = (await Project.create(language=language)).projectId
         if algorithm:
             # Read the algorithm file.
-            with open('tests/algorithms/' + algorithm, 'r') as file:
+            with open('algorithms/' + algorithm, 'r') as file:
                 content = file.read()
             # Update the project to the new algorithm.
             await Files.update(
